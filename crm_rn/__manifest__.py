6e0f4b5f02a294e5a8a0f10520f42b5838220170
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-12-28 19:48:44 +0100

    crm_rn v0

---
+++ crm_rn/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'CRM Roldán Netya',
+    'version': '14.0.1.0.0',
+    'category': 'CRM',
+    'description': u"""
+Objetivos y seguimiento mensual
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'crm',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_partner_views.xml',
+        'views/partner_plan_views.xml',
+        'views/menu_views.xml',
+    ],
+    'installable': True,
+}
---
+++ crm_rn/models/__init__.py
@@ -0,0 +1,1 @@
+from . import objetivo_anual
