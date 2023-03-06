89a77745c7e7aa17ae767fde7099333bc086e1b0
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-03-06 23:59:09 +0100

    crm_rn v0

--- crm_rn/models/objetivo_anual_linea.py
+++ crm_rn/models/objetivo_anual_linea.py
@@ -26,8 +26,8 @@ class ObjetivoAnualLinea(models.Model):
                                           'pero el sistema guarda la información histórica que es la interesante.')
     es_nueva = fields.Boolean('Es nueva', store=True, readonly=True)
     es_objetivo = fields.Boolean('Es objetivo', store=True, readonly=True)
-    es_perdida = fields.Boolean('Es perdida', store=True, readonly=True, related='oportunidad_id.es_perdida',
-                                help='Campo de control para mostrar en las líneas de análisis anual.')
+#    es_perdida = fields.Boolean('Es perdida', store=True, readonly=True, related='oportunidad_id.es_perdida',
+#                                help='Campo de control para mostrar en las líneas de análisis anual.')
     estado = fields.Selection('Estado Op.', store=True, readonly=True, related='oportunidad_id.estado')
     estado_actual_id = fields.Many2one('crm.stage', string="Última etapa", store=False, readonly=True, related='oportunidad_id.stage_id')
     estado_inicial_id = fields.Many2one('crm.stage', string="Estado inicial", store=True, readonly=True)
