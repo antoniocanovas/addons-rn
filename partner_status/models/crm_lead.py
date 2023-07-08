3e3c24a35d6539d75bb0030b33c5fa37389e7c33
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-08 18:20:26 +0200

    crm_rn_v1

--- partner_status/models/crm_lead.py
+++ partner_status/models/crm_lead.py
@@ -4,6 +4,6 @@ from odoo import _, api, fields, models
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    partner_status_id = fields.Many2one('partner.status', related='partner_id.status', store=True)
+    partner_status_id = fields.Many2one('partner.status', related='partner_id.status_id', store=True)
     is_prospection = fields.Boolean('Prospection', related='partner_status_id.is_prospection')
 
