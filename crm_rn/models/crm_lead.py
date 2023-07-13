6ef5b93cdbbd9cf792bd7788515454596b352d00
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-13 12:13:09 +0200

    crm_rn_v1

--- crm_rn/models/crm_lead.py
+++ crm_rn/models/crm_lead.py
@@ -82,7 +82,7 @@ class CrmLead(models.Model):
         DIGITO_CONTROL = "TRWAGMYFPDXBNJZSQVHLCKE"
         INVALIDOS = {"00000000T", "00000001R", "99999999R"}
 
-        if self.vat not in INVALIDOS \
-                and re.match(REGEXP, self.vat) is None \
-                and self.vat[8] == DIGITO_CONTROL[int(self.vat[0:8]) % 23]:
+        if self.vat in INVALIDOS \
+                or re.match(REGEXP, self.vat) is None \
+                or self.vat[8] == DIGITO_CONTROL[int(self.vat[0:8]) % 23]:
                     raise UserWarning('NIF no válido')
