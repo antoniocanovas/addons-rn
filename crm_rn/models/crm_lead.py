e9acd75e78dc7bff12c6992425926959553e115d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-13 13:59:23 +0200

    crm_rn_v1

--- crm_rn/models/crm_lead.py
+++ crm_rn/models/crm_lead.py
@@ -87,7 +87,7 @@ class CrmLead(models.Model):
             raise ValidationError('NIF no válido 1')
         if len(vat) != 9:
             raise ValidationError('Son 9 dígitos')
-        if re.match(REGEXP, vat) == False:
+        if re.match(REGEXP, vat) is None:
             raise ValidationError('NIF no válido 2')
         if self.vat[8] != DIGITO_CONTROL[int(self.vat[:8]) % 23]:
            raise ValidationError('NIF no válido3 ')
